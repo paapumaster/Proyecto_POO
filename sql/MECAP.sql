751a9a40865212bf39b99bdf0a956635d80cf2ba
Sebastian Durán <sebaduran78y@gmail.com>
2026-09-14 13:04:30 -0300

    nuevos cambios sql

--- sql/MECAP.sql
+++ sql/MECAP.sql
@@ -35,3 +35,78 @@ CREATE TABLE direcciones(
     CONSTRAINT fk_direcciones_tipos_direccion FOREIGN KEY (tipo_direccion) REFERENCES tipos_direccion
 
 );
+
+
+ALTER TABLE tipos_direccion COMMENT = 'Tipos de direccion, Domiciliario, laboral, academico, oficina'
+ALTER TABLE comunas COMMENT = 'Datos d ecomunas de chile segun el codigo unico territorial (CUT) de'
+ALTER TABLE direcciones COMMENT = 'Informacion de direcciones para asociar a talleres, clientes'
+ALTER TABLE direcciones ADD habilitado TINYINT NOT NULL DEFAULT 1;
+
+
+CREATE TABLE talleres(
+    id_taller INTEGER AUTO_INCREMENT,
+    nombre_taller VARCHAR(50) NOT NULL,
+    direccion,
+    habilitado TINYINT NOT NULL DEFAULT 1,
+
+    CONSTRAINT pk_talleres PRIMARY KEY (id_taller)
+    COMMENT = 'Informacion de talleres mecanicos';
+
+
+)
+
+
+
+
+CREATE TABLE tipos_mecanicos(
+    id_tipo_mecanico INTEGER AUTO_INCREMENT,
+    tipo_mecanico VARCHAR(25) NOT NULL,
+    detalle VARCHAR(50) NULL,
+    habilitado TINYINT NOT NULL DEFAULT 1,
+
+    CONSTRAINT pk_tipos_mecanicos PRIMARY KEY (id_tipo_mecanico)
+    COMMENT = 'Informacion de tipos de mecanico para a sociar a mecanicos de talleres';
+)
+
+
+CREATE TABLE mecanicos(
+    rut INTEGER NOT NULL UNIQUE,
+    correo VARCHAR(255) NULL,
+    telefono VARCHAR(15) NULL,
+    digito_verificador CMAR(i) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    fecha_contrato DATE NOT NULL,
+    salario DECIMAL NULL,
+    direccion INTEGER,
+    tipo_mecanico INTEGER,
+
+    CONSTRAINT pk_mecanico PRIMARY KEY (rut),
+    CONSTRAINT fk_mecanicos_direcciones FOREIGN KEY (direccion) REFERENCES direcciones(id_direccion)
+    CONSTRAINT fk_mecanicos_tipomecanico FOREIGN KEY (tipo_mecanico) REFERENCES tipos_mecanicos(id_tipo_mecanico)
+) 
+
+
+CREATE TABLE talleres_mecanicos(
+    id_taller_mecanico INTEGER AUTO_INCREMENT,
+    mecanico INTEGER NOT NULL,
+    taller INTEGER NOT NULL,
+    habilitado TINYINT NOT NULL,
+
+    CONSTRAINT pk_talleres_mecanicos PRIMARY KEY (id_taller_mecanico),
+    CONSTRAINT fk_talleresmecanicos_taller FOREIGN KEY (taller) REFERENCES talleres(id_taller),
+    CONSTRAINT fk_talleresmecanicos_mecanico FOREIGN KEY (mecanico) REFERENCES mecanicos(rut),
+    )
+
+
+
+CREATE TABLE parametros(
+    id_parametro INTEGER AUTO_INCREMENT,
+    tipo_parametro VARCHAR(30) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+
+    CONSTRAINT pk_parametros PRIMARY KEY (id_parametro)
+) COMMENT = 'Informacion parametrica usada dentro de la aplicacion: Combustible, tipo d evehiculo, etc';
+
+
+ALTER TABLE parametros ADD habilitado TINYINT NOT NULL DEFAULT 1; 
